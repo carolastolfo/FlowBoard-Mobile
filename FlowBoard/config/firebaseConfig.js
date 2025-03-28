aa44082c210d016446164ba715d0eadf6376d972
FreddyHigaSectionC <156973801+FreddyHigaSectionC@users.noreply.github.com>
2025-03-27 20:21:56 -0400

    Delete config

--- FlowBoard/config/firebaseConfig.js
+++ FlowBoard/config/firebaseConfig.js
@@ -4,12 +4,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCi-pWfbovUgWF7gVU6He0fmuEK31F91pw",
-  authDomain: "winter25-51be3.firebaseapp.com",
-  projectId: "winter25-51be3",
-  storageBucket: "winter25-51be3.firebasestorage.app",
-  messagingSenderId: "889208687119",
-  appId: "1:889208687119:web:cbc1063639bcd97879717a"
+
 };
 
 // Initialize Firebase
