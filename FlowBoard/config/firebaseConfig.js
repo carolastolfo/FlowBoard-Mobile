a6269d26c6ef2515e2c9a035e19b0c4f64c1f136
yeonu <yeonu@Yeonu.local>
2025-03-30 17:07:26 -0400

    Add: join board feature

--- FlowBoard/config/firebaseConfig.js
+++ FlowBoard/config/firebaseConfig.js
@@ -5,12 +5,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
 // Copy the initialization code from firestore and save it here
-apiKey: "AIzaSyCx1fsj_8i0_Hktji9a8B0W0ijwK4gmVkY",
-authDomain: "winter25-63d21.firebaseapp.com",
-projectId: "winter25-63d21",
-storageBucket: "winter25-63d21.firebasestorage.app",
-messagingSenderId: "518327692923",
-appId: "1:518327692923:web:28ffd635ca26d326d6bcde"
+
 };
 
 // Initialize Firebase
